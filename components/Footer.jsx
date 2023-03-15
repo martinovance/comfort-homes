d46d23ba363991c5bd87ebbf3ed9aac61c45302b
unknown <martinsogunsina@gmail.com>
2023-03-15 08:44:42 +0600

    Textual updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 const Footer = () => (
     <Box textAlign="center" p="5" borderTop="1px" borderColor="gray.100">
-        2022 Comfort Homes, Inc
+        Comfort Homes, Inc
     </Box>
 );
 
